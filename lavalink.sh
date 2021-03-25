71db0c9ce15267dd3e5a5a37446dfbbe54c69e0e
Yimura <andreas.maerten@scarlet.be>
2021-03-25 21:34:38 +0100

    fix(Script): sed incorrect formatting

--- lavalink.sh
+++ lavalink.sh
@@ -5,8 +5,8 @@
 ipv6=$(curl -s6 http://icanhazip.com)
 password=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random)
 
-sed -i "s/yourPasswordHer/$password" application.example.yml
-sed -i "s/theIp64RangeHere/$ipv6\/64" application.example.yml
+sed -i "s/yourPasswordHer/$password/" application.example.yml
+sed -i "s/theIp64RangeHere/$ipv6\/64/" application.example.yml
 
 mv application.example.yml application.yml
 mv docker-compose.example.yml docker-compose.yml
