1c9968881ea9e476957e827dbea3cdb34ab767ea
Yimura <andreas.maerten@scarlet.be>
2021-08-25 22:36:49 +0200

    feat(Main): Log stdout & stderr to file

--- main.sh
+++ main.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # main startup script
 
-# check if apt is ready
-while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-    echo "APT is not ready to be used, waiting..."
-    sleep 5
-done
-
 repo="https://github.com/Yimura/Vultr-LavaLink-ServerPrep/archive/refs/heads/main.tar.gz"
 
 wget $repo
@@ -14,5 +8,4 @@ tar xf main.tar.gz
 cd Vultr-LavaLink-ServerPrep-main
 
 chmod a+x ./*.sh
-
-./docker.sh && ./ipv6.sh && ./lavalink.sh
+./start.sh &> setup.log
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# check if apt is ready
+while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    echo "APT is not ready to be used, waiting..."
+    sleep 5
+done
+
+./docker.sh && ./ipv6.sh && ./lavalink.sh
