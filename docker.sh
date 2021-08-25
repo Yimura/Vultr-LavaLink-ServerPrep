cd33b027e35530a6ec32c6add6e3d023251638c1
Yimura <andreas.maerten@scarlet.be>
2021-08-25 23:20:32 +0200

    feat(Docker): Split docker from docker-compose

--- docker.sh
+++ docker.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 # installs docker and docker-compose
 
-curl -fsSL https://get.docker.com | sh && apt update && apt install docker-compose -y
+echo "Installing docker from get.docker.com"
+curl -fsSL https://get.docker.com | sh
+
+echo "Installing docker-compose"
+for i in {1..5}
+do
+    apt update && apt install docker-compose -y
+
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to setup docker-compose, retrying..."
+
+        sleep 1
+
+        continue
+    fi
+
+    break
+done
