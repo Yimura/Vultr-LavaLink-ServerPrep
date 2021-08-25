4375190f8ecc0d71b0640a8684ca20a94d57c6f7
Yimura <andreas.maerten@scarlet.be>
2021-08-25 21:33:01 +0200

    fix(Main): Attempt to wait for apt
    Because of Vultr randomness apt is not immediately available (?)

--- main.sh
+++ main.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # main startup script
 
+# check if apt is ready
+while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    echo "APT is not ready to be used, waiting..."
+    sleep 5
+done
+
 repo="https://github.com/Yimura/Vultr-LavaLink-ServerPrep/archive/refs/heads/main.tar.gz"
 
 wget $repo
